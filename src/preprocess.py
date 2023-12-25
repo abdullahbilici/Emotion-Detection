544d35c1d49404d96ed35a47c911098adb3bdf96
abdullahbilici <abdullahbilici142@gmail.com>
2023-12-25 23:35:15 +0300

    Image preprocessing

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,16 +1,23 @@
 from PIL import Image
 import os
 
-image_directory = "data/angry"
+emotions = ["sad", "angry", "shocked", "happy"]
+
+image_directory = "data/images"
 processed_directory = "data/processed"
 
-for file_name in os.listdir(image_directory):
-    if file_name.endswith(".jpg") or file_name.endswith(".png"): 
-        image_path = os.path.join(image_directory, file_name)
-        with Image.open(image_path) as img:
-            gray_img = img.convert("L")
-            resized_img = gray_img.resize((64, 64))
-            processed_path = os.path.join(processed_directory, file_name)
-            resized_img.save(processed_path)
+for emotion in emotions:
+    emo_img_dir = os.path.join(image_directory, emotion)
+    emo_pro_dir = os.path.join(processed_directory, emotion)
+
+    for file_name in os.listdir(emo_img_dir):
+
+        if file_name.endswith(".jpg") or file_name.endswith(".png"): 
+            image_path = os.path.join(emo_img_dir, file_name)
+            with Image.open(image_path) as img:
+                gray_img = img.convert("L")
+                resized_img = gray_img.resize((128, 128))
+                processed_path = os.path.join(emo_pro_dir, file_name)
+                resized_img.save(processed_path)
 
 print("Image processing complete!")
